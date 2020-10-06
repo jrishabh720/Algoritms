91a5f17d1ae20a923e7f0d4bff17debfef77b179
jrishabh722 <56186120+jrishabh722@users.noreply.github.com>
2020-10-06 23:41:59 +0530

    Time Complexity: O(N * N)

--- Print all prime numbers less than or equal to N.cpp
+++ Print all prime numbers less than or equal to N.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pn(x) cout<<x<<endl;
+#define ps(x) cout<<x<<" ";
+#define n() cout<<endl;
+#define inp(x) cin>>x;
+#define pa(a,n) for(ll i=0;i<n;i++)cout<<a[i]<<" ";
+#define ll long long
+using namespace std;
+                    
+bool isPrime(ll n) 
+{ 
+	if (n <= 1) 
+		return false; 
+	for (ll i = 2; i < n; i++) 
+		if (n % i == 0) 
+			return false; 
+	return true; 
+} 
+void printPrime(ll n) 
+{ 
+	for (ll i = 2; i <= n; i++) { 
+		if (isPrime(i)) 
+			cout << i << " "; 
+	} 
+} 
 
+int main() 
+{ 
+	ll n; 
+	inp(n);
+	printPrime(n); 
+} 
